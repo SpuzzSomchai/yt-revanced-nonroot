45da7a9625694698d5594a591ac41a069997c801
luxysiv <46205571+luxysiv@users.noreply.github.com>
2019-11-04 19:48:49 +0700

    Create nhaccuatui.js

---
+++ nhaccuatui.js
@@ -0,0 +1,10 @@
+var obj = JSON.parse($response.body);
+let url = $request.url;
+var cons = "users/info";
+if(url.indexOf(cons) != -1)
+{
+obj.data.VIPExpire= "29/01/9999 00:00:00";
+obj.data.isVIP= true;
+}
+$done({body: JSON.stringify(obj)});
+
