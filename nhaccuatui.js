5d74aa0f60a879b63e68b65cf4b492363288e87d
luxysiv <46205571+luxysiv@users.noreply.github.com>
2019-11-06 05:47:08 +0700

    Update nhaccuatui.js

--- nhaccuatui.js
+++ nhaccuatui.js
@@ -7,14 +7,5 @@ obj.data.VIPExpire= "29/01/9999 00:00:00";
 obj.data.isVIP= true;
 }
 $done({body: JSON.stringify(obj)});
-var obj = JSON.parse($response.body);
-let url = $request.url;
-var cons = "users/info";
-if(url.indexOf(cons) != -1)
-{
-obj.data.VIPExpire= "29/01/9999 00:00:00";
-obj.data.isVIP= true;
-}
-$done({body: JSON.stringify(obj)});
 
 
