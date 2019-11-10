9b8842d87620371cb35341efd6dd89d531ea30c4
luxysiv <46205571+luxysiv@users.noreply.github.com>
2019-11-10 08:16:54 +0700

    Update zingmp3.vip.js

--- zingmp3.vip.js
+++ zingmp3.vip.js
@@ -1,11 +1,5 @@
-var obj = JSON.parse($response.body);
-var url = $request.url;
-var cons = "getUserInfo";
-if(url.indexOf(cons) != -1)
-{
-obj["data"]["lastVipExpireTime"]= 253378108800;
-obj["data"]["isIapLog"]= false;
-}
+body = $response.body.replace(/"lastVipExpireTime".*\d{10}\n/, "");
+let obj = JSON.parse(body);
+obj["data"]["sessionsKey"]= "sm1.mfc_.1009719336.10.vjnZ26bVD1L1HnifQLyq5MbVD1LfNpGLQDphhpHVD1K";
+obj["data"]["vip"]= {"expireTime":1869254270,"startTime":1572527803,"subscription":{"status":1,"expireTime": 1869254270,"platform":2},"vipType":1};
 $done({body: JSON.stringify(obj)});
-
-// Descriptions
