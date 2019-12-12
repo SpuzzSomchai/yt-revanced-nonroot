431a25c2d28b0842fe77ce5797d3cc30de4dc26b
manhduonghn <manhduonghna@gmail.com>
2019-12-12 08:07:06 +0700

    Update

--- nhaccuatui.js
+++ nhaccuatui.js
@@ -3,7 +3,7 @@ let url = $request.url;
 var cons = "users/info";
 if(url.indexOf(cons) != -1)
 {
-obj.data.VIPExpire= "29/01/9999 00:00:00";
+obj.data.VIPExpire= "29/01/2099 00:00:00";
 obj.data.isVIP= true;
 }
 $done({body: JSON.stringify(obj)});
