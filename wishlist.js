d80920d62f359354230b52a5a9671237d69ab71f
Mạnh Dương <manhduonghn@icloud.com>
2021-10-07 12:40:37 +0700

    Update

--- wishlist.js
+++ wishlist.js
@@ -1,4 +1,4 @@
-const $ = new API(" Wishlist", true);
+const $ = new API("wishlist", true);
 let apps = [];
 if ($.read("apps") != "" && $.read("apps") != undefined) {
   apps = $.read("apps").split("，");
