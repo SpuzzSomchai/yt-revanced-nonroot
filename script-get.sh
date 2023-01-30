68895297fb67ff7b00de989a3cd14b17eaa06ef0
luxysiv <46205571+luxysiv@users.noreply.github.com>
2023-01-30 22:04:27 +0700

    update script-get.sh

--- script-get.sh
+++ script-get.sh
@@ -1,5 +1,6 @@
 readonly -A FILTERS=(
     ['spotify-proto.js']='https://raw.githubusercontent.com/app2smile/rules/master/js/spotify-proto.js'
+    ['resource-parser.js']='https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/Scripts/resource-parser.js'
     ['youtube.js']='https://raw.githubusercontent.com/Maasea/sgmodule/master/Script/Youtube/youtube.js'
 )
 function join_by { local IFS="$1"; shift; echo "$*"; }
