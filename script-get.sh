7d735d6deb4b87c67bca4783e0b91eb4a5b90fa3
luxysiv <46205571+luxysiv@users.noreply.github.com>
2023-01-30 21:40:20 +0700

    Create script-get.sh

---
+++ script-get.sh
@@ -0,0 +1,14 @@
+readonly -A FILTERS=(
+    ['abpvn.txt']='https://raw.githubusercontent.com/abpvn/abpvn/master/filter/abpvn.txt'
+    ['ext.txt']='https://raw.githubusercontent.com/luxysiv/filters/main/ext.txt'
+)
+function join_by { local IFS="$1"; shift; echo "$*"; }
+for filter in "${!FILTERS[@]}"; do
+    url="${FILTERS[$filter]}"
+    echo "Downloading $filter from $url"
+    if ! curl -o "$filter" "$url"; then
+        echo "Failed to download $url"
+        exit 1
+    fi
+    echo
+done
