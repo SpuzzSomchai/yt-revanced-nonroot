0621b13449d4771322bec047a2dfa24d120f2226
luxysiv <46205571+luxysiv@users.noreply.github.com>
2023-01-31 09:50:28 +0700

    Update script-get.sh

--- script-get.sh
+++ script-get.sh
@@ -1,7 +1,7 @@
 readonly -A FILTERS=(
     ['spotify-proto.js']='https://raw.githubusercontent.com/app2smile/rules/master/js/spotify-proto.js'
     ['resource-parser.js']='https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/Scripts/resource-parser.js'
-    ['youtube.js']='https://raw.githubusercontent.com/Maasea/sgmodule/master/Script/Youtube/youtube.js'
+    ['youtube-premium.js']='https://raw.githubusercontent.com/Maasea/sgmodule/master/Script/Youtube/youtube.js'
 )
 function join_by { local IFS="$1"; shift; echo "$*"; }
 for filter in "${!FILTERS[@]}"; do
