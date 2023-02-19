79290c536e4211c16890a650a5bc3c593c20381a
luxysiv <46205571+luxysiv@users.noreply.github.com>
2023-02-19 22:03:12 +0700

    Excluded-patches

--- build-rv.sh
+++ build-rv.sh
@@ -120,3 +120,4 @@ java -jar revanced-cli.jar -a youtube.apk -b revanced-patches.jar -m revanced-in
 apksigner="$(find $ANDROID_SDK_ROOT/build-tools -name apksigner | sort -r | head -n 1)"
 # Sign apks (https://github.com/tytydraco/public-keystore)
 ${apksigner} sign --ks public.jks --ks-key-alias public --ks-pass pass:public --key-pass pass:public --in ./revanced.apk --out ./revanced_signed.apk
+
