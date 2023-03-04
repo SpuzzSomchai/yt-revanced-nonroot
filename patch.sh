1bd7475a3a5ba224a7b056b82874a9c26cf1bf72
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-03-04 16:14:26 +0700

    update patch.sh

--- patch.sh
+++ patch.sh
@@ -7,7 +7,42 @@ do
 source $var
 
 # Begin
-echo "🎊 STARTING PATCH ${NAME}..."
+WGET_HEADER="User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0"
+DATE=$(date +%y%m%d)
+DRAFT=false
+if [ x${1} == xtest ]; then DRAFT=true; fi
+
+req() {
+    wget -q -O "$2" --header="$WGET_HEADER" "$1"
+}
+
+get_latestytversion() {
+    url="https://www.apkmirror.com/apk/google-inc/youtube/"
+    YTVERSION=$(req "$url" - | grep "All version" -A200 | grep app_release | sed 's:.*/youtube-::g;s:-release/.*::g;s:-:.:g' | sort -r | head -1)
+    echo "Latest Youtube Version: $YTVERSION"
+}
+
+dl_yt() {
+    rm -rf $2
+    echo "🚘 Downloading YouTube $1"
+    url="https://www.apkmirror.com/apk/google-inc/youtube/youtube-${1//./-}-release/"
+    url="$url$(req "$url" - | grep Variant -A50 | grep ">APK<" -A2 | grep android-apk-download | sed "s#.*-release/##g;s#/\#.*##g")"
+    url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
+    url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
+    req "$url" "$2"
+}
+# Fetch latest official supported YT versions
+curl -s https://api.github.com/repos/${USER}/revanced-patches/releases/latest \
+| grep "browser_download_url.*json" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+mv patches.json ${NAME}-patches.json
+YTVERSION=$(jq -r '.[] | select(.name == "microg-support") | .compatiblePackages[] | select(.name == "com.google.android.youtube") | .versions[-1]' ${NAME}-patches.json)
+rm -rf ${NAME}-patches.json
+
+# Download Youtube
+dl_yt $YTVERSION youtube-v$1.apk
 
 # Get patches 
 echo "⏭️ Prepairing ${NAME} patches..."
@@ -36,77 +71,20 @@ curl -s https://api.github.com/repos/${USER}/revanced-integrations/releases/late
 | wget -qi -
 mv revanced-integrations*.apk ${NAME}-integrations.apk
 
-# Repair
-declare -A apks
-apks["youtube-mirror.apk"]=dl_yt
-
-## Functions
-# Wget user agent
-WGET_HEADER="User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0"
-
-# Wget function
-req() { wget -nv -O "$2" --header="$WGET_HEADER" "$1"; }
-
-# Wget apk verions
-get_apk_vers() { req "$1" - | sed -n 's;.*Version:</span><span class="infoSlide-value">\(.*\) </span>.*;\1;p'; }
-
-# Wget apk verions(largest)
-get_largest_ver() {
-	local max=0
-	while read -r v || [ -n "$v" ]; do
-		if [[ ${v//[!0-9]/} -gt ${max//[!0-9]/} ]]; then max=$v; fi
-	done
-	if [[ $max = 0 ]]; then echo ""; else echo "$max"; fi
-}
-
-# Wget download apk
-dl_apk() {
-	local url=$1 regexp=$2 output=$3
-	url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n "s/href=\"/@/g; s;.*${regexp}.*;\1;p")"
-	echo "$url"
-	url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
-	url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
-	req "$url" "$output"
-}
-
-# Downloading youtube
-dl_yt() {
-	local last_ver
-	last_ver="$version"
-	last_ver="${last_ver:-$(get_apk_vers "https://www.apkmirror.com/uploads/?appcategory=youtube" | get_largest_ver)}"
-
-	echo "⏭️ Choosing version '${last_ver}'"
-	local base_apk="youtube-v${VERSION}.apk"
-	if [ ! -f "$base_apk" ]; then
-		declare -r dl_url=$(dl_apk "https://www.apkmirror.com/apk/google-inc/youtube/youtube-${last_ver//./-}-release/" \
-			"APK</span>[^@]*@\([^#]*\)" \
-			"$base_apk")
-	fi
-}
-
-## Main
-for apk in "${!apks[@]}"; do
-    if [ ! -f $apk ]; then
-        echo "⏭️ Downloading $apk"
-        version=${VERSION}
-        ${apks[$apk]}
-    fi
-done
-
 # Patch revanced and revanced extended
 echo "⏭️ Patching YouTube..."
-java -jar ${NAME}-cli.jar -a youtube-v${VERSION}.apk -b ${NAME}-patches.jar -m ${NAME}-integrations.apk -o ${NAME}.apk ${INCLUDE_PATCHES} ${EXCLUDE_PATCHES} -c 2>&1 | tee -a patchlog.txt
+java -jar ${NAME}-cli.jar -a youtube-v$1.apk -b ${NAME}-patches.jar -m ${NAME}-integrations.apk -o ${NAME}.apk ${INCLUDE_PATCHES} ${EXCLUDE_PATCHES} -c 2>&1 | tee -a patchlog.txt
 
 # Find and select apksigner binary
-echo "⏭️ Signing ${NAME}-v${VERSION}..."
+echo "⏭️ Signing ${NAME}-v${YTVERSION}..."
 apksigner="$(find $ANDROID_SDK_ROOT/build-tools -name apksigner | sort -r | head -n 1)"
 
 # Sign apks (https://github.com/tytydraco/public-keystore)
-${apksigner} sign --ks public.jks --ks-key-alias public --ks-pass pass:public --key-pass pass:public --in ./${NAME}.apk --out ./yt-${NAME}-v${VERSION}.apk
+${apksigner} sign --ks public.jks --ks-key-alias public --ks-pass pass:public --key-pass pass:public --in ./${NAME}.apk --out ./yt-${NAME}-v${YTVERSION}.apk
 
 # Refresh patches cache
 echo "⏭️ Clean patches cache..."
-rm -f *-cli.jar *-integrations.apk *-patches.jar
+rm -f *-cli.jar *-integrations.apk *-patches.jar youtube-patch.apk
 
 # Finish
 done
