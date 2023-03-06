87a3304cd4ff9a836a07ba0c03a4266499ac8323
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-03-06 07:16:49 +0700

    update patch.sh

--- patch.sh
+++ patch.sh
@@ -5,7 +5,10 @@
 for var in patch-rv.txt patch-rve.txt
 do
 source $var
+
+# Prepair patches keywords
 patch_file=${PATCH}
+
 # Get line numbers where included & excluded patches start from. 
 # We rely on the hardcoded messages to get the line numbers using grep
 excluded_start="$(grep -n -m1 'EXCLUDE PATCHES' "$patch_file" | cut -d':' -f1)"
@@ -33,7 +36,7 @@ populate_patches() {
 [[ ! -z "$excluded_patches" ]] && populate_patches "-e" "$excluded_patches"
 [[ ! -z "$included_patches" ]] && populate_patches "-i" "$included_patches"
 
-# Begin
+# Download latest APK supported
 WGET_HEADER="User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0"
 DATE=$(date +%y%m%d)
 DRAFT=false
@@ -51,28 +54,26 @@ get_latestytversion() {
 
 dl_yt() {
     rm -rf $2
-    echo "🚘 Downloading YouTube v$1"
+    echo -e "🚘 Downloading YouTube v$1..."
     url="https://www.apkmirror.com/apk/google-inc/youtube/youtube-${1//./-}-release/"
     url="$url$(req "$url" - | grep Variant -A50 | grep ">APK<" -A2 | grep android-apk-download | sed "s#.*-release/##g;s#/\#.*##g")"
     url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
     url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
     req "$url" "$2"
 }
-# Fetch latest official supported YT versions
+# Fetch latest supported YT versions
 curl -s https://api.github.com/repos/${USER}/revanced-patches/releases/latest \
 | grep "browser_download_url.*json" \
 | cut -d : -f 2,3 \
 | tr -d \" \
 | wget -qi -
-mv patches.json ${NAME}-patches.json
-YTVERSION=$(jq -r '.[] | select(.name == "microg-support") | .compatiblePackages[] | select(.name == "com.google.android.youtube") | .versions[-1]' ${NAME}-patches.json)
-rm -rf ${NAME}-patches.json
+YTVERSION=$(jq -r '.[] | select(.name == "microg-support") | .compatiblePackages[] | select(.name == "com.google.android.youtube") | .versions[-1]' patches.json)
 
 # Download Youtube
 dl_yt $YTVERSION youtube-v${YTVERSION}.apk
 
 # Get patches 
-echo "⏭️ Prepairing ${NAME} patches..."
+echo -e "⏭️ Prepairing ${NAME} patches..."
 
 # Revanced-patches
 curl -s https://api.github.com/repos/${USER}/revanced-patches/releases/latest \
@@ -80,7 +81,6 @@ curl -s https://api.github.com/repos/${USER}/revanced-patches/releases/latest \
 | cut -d : -f 2,3 \
 | tr -d \" \
 | wget -qi -
-mv revanced-patches*.jar ${NAME}-patches.jar
 
 # Revanced CLI
 curl -s https://api.github.com/repos/${USER}/revanced-cli/releases/latest \
@@ -88,7 +88,6 @@ curl -s https://api.github.com/repos/${USER}/revanced-cli/releases/latest \
 | cut -d : -f 2,3 \
 | tr -d \" \
 | wget -qi -
-mv revanced-cli*.jar ${NAME}-cli.jar
 
 # ReVanced Integrations
 curl -s https://api.github.com/repos/${USER}/revanced-integrations/releases/latest \
@@ -96,24 +95,23 @@ curl -s https://api.github.com/repos/${USER}/revanced-integrations/releases/late
 | cut -d : -f 2,3 \
 | tr -d \" \
 | wget -qi -
-mv revanced-integrations*.apk ${NAME}-integrations.apk
 
-# Patch revanced and revanced extended
-echo "⏭️ Patching YouTube..."
-java -jar ${NAME}-cli.jar -m ${NAME}-integrations.apk -b ${NAME}-patches.jar \
+# Patch APK
+echo -e "⏭️ Patching YouTube..."
+java -jar revanced-cli*.jar -m revanced-integrations*.apk -b revanced-patches*.jar \
    ${patches[@]} \
     -a youtube-v${YTVERSION}.apk -o ${NAME}.apk
 
 # Find and select apksigner binary
-echo "⏭️ Signing ${NAME}-v${YTVERSION}..."
+echo -e "⏭️ Signing ${NAME}-v${YTVERSION}..."
 apksigner="$(find $ANDROID_SDK_ROOT/build-tools -name apksigner | sort -r | head -n 1)"
 
 # Sign apks (https://github.com/tytydraco/public-keystore)
 ${apksigner} sign --ks public.jks --ks-key-alias public --ks-pass pass:public --key-pass pass:public --in ./${NAME}.apk --out ./yt-${NAME}-v${YTVERSION}.apk
 
 # Refresh patches cache
-echo "⏭️ Clean patches cache..."
-rm -f *-cli.jar *-integrations.apk *-patches.jar 
+echo -e "⏭️ Clean patches cache..."
+rm -f revanced-cli*.jar revanced-integrations*.apk revanced-patches*.jar patches.json
 
 # Finish
 done
