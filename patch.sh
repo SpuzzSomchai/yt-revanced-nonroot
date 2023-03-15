06909cd7b85ea627ea32e87989a90807059989a1
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-03-15 16:23:01 +0700

    Revanced build

--- patch.sh
+++ patch.sh
@@ -129,7 +129,7 @@ java -jar revanced-cli*.jar \
      -b revanced-patches*.jar \
      -a youtube-v$YTVERSION.apk \
      ${patches[@]} \
-     --keystore=r.keystore -p=revanced \
+     --keystore=ks.keystore \
      -o yt-$NAME-v$YTVERSION.apk
 
 # Refresh patches cache
