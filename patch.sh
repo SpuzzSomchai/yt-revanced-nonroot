55d4282ad7a44a2f8110d5d08111089f6b6e9854
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-04-01 05:08:09 +0700

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -69,7 +69,7 @@ curl -s "https://api.github.com/repos/$USER/revanced-cli/releases/latest" \
 curl -s "https://api.github.com/repos/$USER/revanced-integrations/releases/latest" \
 | jq -r '.assets[].browser_download_url'  
 }
-urls_res | wget -qi -
+urls_res | sed 's/^/-O /' | xargs curl -s -JL
 
 # Download YouTube APK supported
 WGET_HEADER="User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:111.0) Gecko/20100101 Firefox/111.0"
