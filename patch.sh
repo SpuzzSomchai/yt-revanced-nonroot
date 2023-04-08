d22478fb98727fa1c0d019f1d55ccc636eb5e78b
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-04-08 21:02:10 +0700

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -11,7 +11,7 @@ revanced_ytversion="" # Input version supported. Ex: 18.03.36
 revanced_extended_name="revanced-extended"
 revanced_extended_user="inotia00"
 revanced_extended_patch="patches.rve"
-#revanced_extended_ytversion="" # Input version supported. Ex: 18.07.35
+revanced_extended_ytversion="" # Input version supported. Ex: 18.07.35
 
 # Function prepare patches keywords
 get_patch() {
