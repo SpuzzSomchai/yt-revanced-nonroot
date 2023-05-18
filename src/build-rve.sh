198ca08633169cd05999f96913872d7db007c77c
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-05-18 22:17:33 +0700

    Update build-rve.sh

--- src/build-rve.sh
+++ src/build-rve.sh
@@ -5,16 +5,16 @@ release=$(curl -s "https://api.github.com/repos/inotia00/revanced-patches/releas
 asset=$(echo "$release" | jq -r '.assets[] | select(.name | test("revanced-patches.*\\.jar$")) | .browser_download_url')
 curl -sL -O "$asset"
 ls revanced-patches*.jar >> new.txt
-rm -f revanced-patches*.jar
+rm -f ./revanced-patches*.jar
 release=$(curl -s "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/latest")
 asset=$(echo $release | jq -r '.assets[] | select(.name == "revanced-extended-version.txt") | .browser_download_url')
 curl -sL -O "$asset"
 if diff -q revanced-extended-version.txt new.txt >/dev/null ; then
-rm -f *.txt
+rm -f ./*.txt
 echo "Old patch!!! Not build"
 exit 0
 else
-rm -f *.txt
+rm -f ./*.txt
 dl_gh "inotia00" "revanced-patches revanced-cli revanced-integrations" "latest"
 # Patch YouTube Extended
 get_patches_key "youtube-revanced-extended"
