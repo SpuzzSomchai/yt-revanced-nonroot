310e9ed133c1b63dee0fd9bba85163580902552a
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-05-18 23:26:53 +0700

    Update build-rve.sh

--- src/build-rve.sh
+++ src/build-rve.sh
@@ -1,32 +1,20 @@
 #!/bin/bash
 # Revanced Extended build
+source ./src/tools.sh
 release=$(curl -s "https://api.github.com/repos/inotia00/revanced-patches/releases/latest")
-asset=$(echo $release | python -c '
-import json, sys
-data = json.load(sys.stdin)
-for asset in data["assets"]:
-    if asset["name"].startswith("revanced-patches") and asset["name"].endswith(".jar"):
-        print(asset["browser_download_url"])
-')
-curl -sLO $asset
+asset=$(echo "$release" | jq -r '.assets[] | select(.name | test("revanced-patches.*\\.jar$")) | .browser_download_url')
+curl -sL -O "$asset"
 ls revanced-patches*.jar >> new.txt
-rm -f ./revanced-patches*.jar
-
+rm -f revanced-patches*.jar
 release=$(curl -s "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/latest")
-asset=$(echo $release | python -c '
-import json, sys
-data = json.load(sys.stdin)
-for asset in data["assets"]:
-    if asset["name"] == "revanced-extended-version.txt":
-        print(asset["browser_download_url"])
-')
+asset=$(echo $release | jq -r '.assets[] | select(.name == "revanced-extended-version.txt") | .browser_download_url')
 curl -sL -O "$asset"
 if diff -q revanced-extended-version.txt new.txt >/dev/null ; then
-rm -f ./*.txt
+rm -f *.txt
 echo "Old patch!!! Not build"
 exit 0
 else
-rm -f ./*.txt
+rm -f *.txt
 dl_gh "inotia00" "revanced-patches revanced-cli revanced-integrations" "latest"
 # Patch YouTube Extended
 get_patches_key "youtube-revanced-extended"
